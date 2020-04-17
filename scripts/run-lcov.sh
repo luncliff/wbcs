d30102a821e0535b34340ff1089ff2100fb95e33
luncliff <luncliff@gmail.com>
2020-04-17 13:25:16 +0900

    fix lcov script's exclude

--- scripts/run-lcov.sh
+++ scripts/run-lcov.sh
@@ -13,18 +13,24 @@
 #       cmake --build .
 #       ctest
 #   popd
-#   bash ./scripts/run-lcov.sh build   # <--- this file
+#   bash ./scripts/run-lcov.sh build ${save_dir}  # <--- this file
+#   open ${save_dir}/index.html
 #
 build_dir=${1:-"build"}
 save_dir=${2:-"docs/coverage"}
 info_file=${3:-"coverage.info"}
 
+# for fpath in $(find $(pwd) -name *.gcda)
+# do
+#     gcov -abcf ${fpath}
+# done
 lcov --directory ${build_dir} --capture --output-file ${info_file}
 
-lcov --remove ${info_file} '/usr/*'          --output-file ${info_file}
-lcov --remove ${info_file} './externals/*'   --output-file ${info_file}
-lcov --remove ${info_file} '*_test.cpp'      --output-file ${info_file}
-lcov --list ${info_file}
+lcov --remove ${info_file} '*/usr/*'        --output-file ${info_file}
+lcov --remove ${info_file} '*/externals/*'  --output-file ${info_file}
+
+lcov --remove ${info_file} '*_test.cpp'     --output-file ${info_file}
+lcov --list   ${info_file}
 
 mkdir -p ${save_dir}
 genhtml ${info_file} -o ${save_dir}
